ebd8f04ce391e0cb70ca508bc6ab4a51a128cbee
Ho Ngoc Bao Tran <hngbtran@gmail.com>
2026-05-30 19:38:26 +0700

    refactor: rename component folders to lowercase

--- frontend/src/layouts/EventLayout.jsx
+++ frontend/src/layouts/EventLayout.jsx
@@ -1,5 +1,5 @@
-import Navbar from '../components/Navbar/Navbar'
-import Sidebar from '../components/Sidebar/Sidebar'
+import Navbar from '../components/navbar/Navbar'
+import Sidebar from '../components/sidebar/Sidebar'
 import styles from './EventLayout.module.css'
 
 function EventLayout({ children }) {
