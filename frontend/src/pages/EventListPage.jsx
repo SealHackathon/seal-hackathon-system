f5df9e547d2f68f7df44a223df48f74b9ba019af
Ho Ngoc Bao Tran <hngbtran@gmail.com>
2026-06-30 07:21:32 +0700

    feat: add layout for EventListPage

--- frontend/src/pages/EventListPage.jsx
+++ frontend/src/pages/EventListPage.jsx
@@ -4,6 +4,7 @@ import EventCard from '../components/coordinator/EventCard'
 import Button from '../components/shared/Button'
 import SectionHeader from '../components/shared/SectionHeader'
 import SearchFilterBar from '../components/shared/SearchFilterBar/SearchFilterBar'
+import CoordinatorLayout from '../layouts/CoordinatorLayout'
 import styles from './EventListPage.module.css'
 import axiosClient from '../api/axiosClient'
 import { useNavigate } from 'react-router-dom'
@@ -247,6 +248,9 @@ function EventListPage({ onManageEvent }) {
   }, [events]) // Đổi sang dependency là [events]
 
   return (
+    <CoordinatorLayout>
+
+    
     <div className={styles.page}>
 
       {/* Header */}
@@ -303,6 +307,7 @@ function EventListPage({ onManageEvent }) {
         }
       </div>
     </div>
+    </CoordinatorLayout>
   )
 }
 
