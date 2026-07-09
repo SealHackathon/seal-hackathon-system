1c53290c5ec1b31347d94afd705b568a56494bf9
Mtung0603 <mtung638@gmail.com>
2026-07-10 00:22:46 +0700

    test

--- backend/seal-hackathon-system/src/main/java/com/minhtung/hackathon/controller/SubmissionController.java
+++ backend/seal-hackathon-system/src/main/java/com/minhtung/hackathon/controller/SubmissionController.java
@@ -125,6 +125,7 @@ public class SubmissionController {
     ) {
         return ResponseEntity.ok(
                 submissionService.viewSubmissionTrackResponses(trackId)
+                //hehe
         );
     }
 }
